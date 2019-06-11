87ee3fe5a4be38deee9b00cce5ed6c821313c4c0
OWIII <songlab.ru@gmail.com>
2019-06-11 08:08:35 +0300

    Yes

--- Test/test.js
+++ Test/test.js
@@ -1,3 +1,10 @@
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode)
-});
+var time=3;
+var first_bicycle=12;
+var second_bicycle=15;
+
+var s1=first_bicycle*time;
+var s2=second_bicycle*time;
+
+var s_together=s1+s2;
+alert(s_together);
+
