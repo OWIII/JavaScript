efd480103722f0792a7f3b9c0fd087f059aea958
OWIII <songlab.ru@gmail.com>
2019-06-17 08:13:36 +0300

    +

---
+++ HW/hw.js
@@ -0,0 +1,41 @@
+console.log("Hello, world");
+console.log("My second console message");
+console.log("What Is Dead May Never Die");
+
+// Comments # 1
+
+//Comments # 2
+
+console.log(3 ** 4);
+
+console.log(6 - (-81));
+
+console.log((Infinity + Infinity) / 2);
+
+console.log((Infinity / Infinity) / 2);
+
+console.log((5 ** 2) - (3 * 7));
+
+// Function
+
+var obj = {
+    prop: "Hi, world"
+};
+var fu = function() {};
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+obj.prop = "Goodbuy";
+
+console.log(obj);
+console.log(typeof fu);
+console.log(typeof obj);
+console.log(typeof arr);
+
+arr[2] = 24;
+console.log(arr);
+
+// Str
+
+str = "hi, world";
+
+console.log(str.toUpperCase());
---
+++ WashItem/test.js
@@ -0,0 +1,8 @@
+var WashItems = function (ItemsLeft) {
+    while (ItemsLeft-- > 0) {
+        alert("В раковине осталось " + ItemsLeft + " предметов");
+    }
+
+};
+WashItems(10);
+
