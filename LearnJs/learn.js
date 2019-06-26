3afaf245fbf889aea9a4186579595cf2f4718fd7
OWIII <songlab.ru@gmail.com>
2019-06-26 07:34:06 +0300

    +

---
+++ LearnJs/learn.js
@@ -0,0 +1,32 @@
+// alert("Hi, man");
+
+//prompt("What are you doing?");
+
+//confirm('Question');
+
+// If, else, ?, :
+
+/*
+
+var answer = prompt('Каково официальное название JavaScript?', '');
+if (answer === 'EcmaScript') {
+    alert("Правильно");
+} else alert("Неверно");
+
+//
+
+(a + b < 4) ? result = 'Мало' : result = 'Много';
+
+
+
+var message;
+
+(login == 'Вася') ? message = 'Привет' :
+    (login == 'Директор') ? message = 'Здравствуйте' :
+ (login == '') ? message = 'Нет логина' : message = '';
+
+/*
+ */
+
+
+
