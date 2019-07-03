c58fc2caa4c1e8020d57fb29d87d67c16626eb55
OWIII <songlab.ru@gmail.com>
2019-07-04 00:15:50 +0300

    Add

--- Training tasks/scripts.js
+++ Training tasks/scripts.js
@@ -1,17 +1,53 @@
-function betterThanAverage(classPoints, yourPoints) {
-    let sumPoints = 0;
-    for (i = 0; i < classPoints.length; i++) {
-        sumPoints += classPoints[i];
-    }
-    let average = sumPoints / classPoints.length;
-    if (yourPoints > average)
-    {
-        console.log(true);
-    }
-    else
-        {
-        console.log(false);
-        }
-}
+// function betterThanAverage(classPoints, yourPoints) {
+//     let sumPoints = 0;
+//     for (i = 0; i < classPoints.length; i++) {
+//         sumPoints += classPoints[i];
+//     }
+//     let average = sumPoints / classPoints.length;
+//     if (yourPoints > average)
+//     {
+//         console.log(true);
+//     }
+//     else
+//         {
+//         console.log(false);
+//         }
+// }
+//
 
-betterThanAverage([2, 3], 2);
+// Objects
+//
+// let obj = {
+//     width : 'str',
+//     func : function() {
+//         console.log( this);
+//     }
+// };
+//  obj.func();
+
+//
+//
+// obj.newProperties = 'Monkey';
+//
+// console.log( obj.newProperties);
+//
+// let a = {prot : 2}, b = {prot : 3}, c = {prot : 5};
+//
+// a = b = c = { tolken : 'AddProp'};
+//
+// // Delete properties
+//
+// delete b.tolken;
+// console.log(a, b, c);
+
+var monkey = 987;
+
+
+
+
+
+
+
+
+
+
