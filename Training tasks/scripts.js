0f1efb7db775260744d43f0a57f6e05d4b485574
OWIII <songlab.ru@gmail.com>
2019-07-06 12:15:17 +0300

    +

--- Training tasks/scripts.js
+++ Training tasks/scripts.js
@@ -66,15 +66,84 @@
 //     console.log(i);
 // };
 
-let obj = {
-    propp : 'one',
-    proppSecond : 'two'
-};
-var prod;
+// let obj = {
+//     propp : 'one',
+//     proppSecond : 'two'
+// };
+// var prod;
+//
+// for ( prod in obj) {
+//     console.log( prod + ' : ' + obj[prod]);
+// }
 
-for ( prod in obj) {
-    console.log( prod + ' : ' + obj[prod]);
-}
+
+//var foo = 'String';
+
+//console.log(navigator.userAgent);
+
+//
+
+//alert(screen.width + ' \n' + screen.height );
+
+//console.log(location.toString());
+
+// DOM
+
+// (function() {
+//     let elems = document.getElementsByTagName('p'); // NodeList
+//     console.log(elems);
+//
+//     for (let i = 0, len = elems.length; i < len; i++) {
+//         console.log(elems[i].tagName);
+//     }
+// })();
+
+//let func = () => {
+    // let elemsByClass = document.getElementsByClassName('paragrah');
+    // let elemById = document.getElementById('four');
+    //let elemSelector = document.querySelectorAll(' div p'); // getElementsByClassName работает быстрее
+    //let elemSelector = document.querySelector('#four');
+    //console.log(elemSelector);
+//};
+
+//func();
+
+// function oddCount(n){
+//     let arr = [];
+//     let i = 0;
+//     arr[i] = n - 2;
+//     n = n - 2;
+//     let len;
+//
+//     while (n > 1)
+//     {
+//         n = n - 2;
+//         arr[i+1] = n;
+//         i = i + 1;
+//     }
+//     len = arr.length;
+//     return len;
+// };
+
+// Easy
+
+// function oddCount(n){
+//     let num = Math.floor(n / 2);
+//     console.log(num);
+// };
+//
+// oddCount(15);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
