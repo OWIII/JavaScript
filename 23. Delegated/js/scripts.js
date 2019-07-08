1c29ebf7bd643f03fe4f1b29b357b9b14b65c2b9
OWIII <songlab.ru@gmail.com>
2019-07-08 23:07:52 +0300

    ++

--- 23. Delegated/js/scripts.js
+++ 23. Delegated/js/scripts.js
@@ -1,13 +1,37 @@
-(function () {
-    let cli= document.getElementById('html');
+// (function () {
+//     let cli= document.getElementById('html');
+//
+//     let func = function() {
+//         alert('Hi');
+//     };
+//
+//     cli.addEventListener('click', func, false);
+//
+// })();
 
-    let func = function() {
-        alert('Hi');
-    };
 
-    cli.addEventListener('click', func, false);
+// let maps = [1, 2, 3];
+//
+// let arraySquared = maps.map(function(num) {
+//     return num * 2;
+// });
+//
+// console.log(arraySquared);
 
-})();
 
+// function maps(x){
+//     let arraySquared = maps.map(function(num) {
+//         return num * 2;
+//     });
+// };
+//
+// maps([1, 2, 3]);
 
+function maps(x){
+    let arraySquared = x.map(function(num) {
+        return num * 2;
+    });
+    return arraySquared;
+};
 
+console.log(maps([1, 2, 3]));
