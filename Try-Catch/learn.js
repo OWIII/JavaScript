b97b92729b63cd534d26c2c3bdf2085904284b74
OWIII <songlab.ru@gmail.com>
2019-07-13 19:58:45 +0300

    Add new tasks

--- Try-Catch/learn.js
+++ Try-Catch/learn.js
@@ -1,32 +1,26 @@
-// alert("Hi, man");
-
-//prompt("What are you doing?");
-
-//confirm('Question');
-
-// If, else, ?, :
-
-/*
-
-var answer = prompt('Каково официальное название JavaScript?', '');
-if (answer === 'EcmaScript') {
-    alert("Правильно");
-} else alert("Неверно");
-
-//
-
-(a + b < 4) ? result = 'Мало' : result = 'Много';
+const need_div_zero = 1;
+const should_div_b = 1;
 
 
+function div(a, b) {
+    try {
+        if (b === 0) {
+            throw new Error(need_div_zero);
+        }
+        if (b === undefined) {
+            throw new Error(should_div_b);
+        }
+    } catch (e) {
+        if (e.message === need_div_zero) {
+            console.error('На нуль делить нельзя');
+        } else if (e.message === should_div_b){
+            console.error('Необходимо передать b');
+        }
+    }
+return a / b;
+}
 
-var message;
-
-(login == 'Вася') ? message = 'Привет' :
-    (login == 'Директор') ? message = 'Здравствуйте' :
- (login == '') ? message = 'Нет логина' : message = '';
-
-/*
- */
+console.log(div(14, 0));
 
 
 
