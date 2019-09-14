7f82dceac89efccf4db0a785e1e0bc86966b4d54
OWIII <owiii.dev@gmail.com>
2019-09-14 16:36:02 +0300

    Promise

--- Async-img/scripts.js
+++ Async-img/scripts.js
@@ -2,19 +2,41 @@ var url1 = 'https://cdn130.picsart.com/298196669081201.jpg?c256x256';
 var url2 = 'https://cdn158.picsart.com/224543536007202.jpg?c256x256';
 var url3 = 'https://pbs.twimg.com/profile_images/421951774203731970/rzhs1Qtc.jpeg';
 
-var image1 = document.createElement('img');
-image1.src = url1;
-document.body.appendChild(image1);
 
-image1.addEventListener('load', () => {
-   console.log('Картинка загружена');
-});
+function loadImages(url) {
+  return new Promise(function (resolve) {
+    var image = document.createElement('img');
+    image.src = url;
+    document.body.appendChild(image);
 
-var image2 = document.createElement('img');
-image2.src = url2;
-document.body.appendChild(image2);
+    image.addEventListener('load', () => {
+      resolve();
+    });
+  });
+}
 
-var image3 = document.createElement('img');
-image3.src = url3;
-document.body.appendChild(image3);
+loadImages(url1)
+    .then(function () {
+      console.log('Картинка 1 загружена');
+      return loadImages(url2);
+
+    })
+    .then(function () {
+      console.log('Картинка 2 загружена');
+      return loadImages(url3);
+    })
+    .then(function () {
+      console.log('Картинка 3 загружена');
+    });
+
+
+
+
+
+
+
+
+
+
+
 
